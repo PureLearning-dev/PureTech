6ee2ed1e683bf636b498410fa2ff2cfd3a9f88c6
PureLearning-dev <liu.jie.coder@gmail.com>
2026-04-21 21:30:20 +0800

    feat(web): Add web module package documentation - Create package-info.java for web module - Prepare web layer for REST controllers and request handling - Set up structure for API endpoints and web configurations

---
+++ puretech-web/src/main/java/com/purelearning/web/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * puretech-web 模块
+ * 项目启动入口，负责处理 HTTP 请求，包括 Controller、拦截器、MVC 配置等
+ * 依赖所有子模块，是整个项目的最顶层模块
+ *
+ * @author PureLearning
+ * @date 2026/4/21
+ */
+
+package com.purelearning.web;
