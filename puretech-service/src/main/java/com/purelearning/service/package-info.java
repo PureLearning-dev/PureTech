4c47d733753c78d81b1249ee3362e65c26937caf
PureLearning-dev <liu.jie.coder@gmail.com>
2026-04-21 21:30:14 +0800

    feat(service): Add service module package documentation - Create package-info.java for service module - Prepare service layer for business logic implementations - Set up structure for user, article, comment, and message services

---
+++ puretech-service/src/main/java/com/purelearning/service/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * puretech-service 模块
+ * 存放所有业务逻辑实现和数据库操作，包括 Service、Mapper 等
+ * 依赖 puretech-api 和 puretech-core 模块
+ *
+ * @author PureLearning
+ * @date 2026/4/21
+ */
+
+package com.purelearning.service;
